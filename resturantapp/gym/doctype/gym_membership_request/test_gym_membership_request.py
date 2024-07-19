70b4c1d8737a8af583ac03ebe5357d7be44340dc
Maaz Tariq <maazt8546@gmail.com>
2024-07-19 16:46:45 +0500

    Initial Commit

---
+++ resturantapp/gym/doctype/gym_membership_request/test_gym_membership_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Maaz Tariq and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGymMembershipRequest(FrappeTestCase):
+	pass
